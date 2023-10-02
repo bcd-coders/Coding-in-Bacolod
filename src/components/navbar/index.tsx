104452e3dcdad3709500217f47901c70b126eff3
Awesome0729 <55270553+Awesome0729@users.noreply.github.com>
2023-10-02 22:50:28 +0800

    Added navbar for the website

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,19 @@
+import '@/style.css'
+
+const index = () => {
+
+
+  return (
+
+    <div className="navbar">
+        <div className="logo">
+            Logo
+        </div>
+        <div className="aboutus">
+            about us
+        </div>
+    </div>
+  )
+}
+
+export default index
