3f3681e9b690237b3a34775e0c04090f7ea78acc
Lanchuii <74664641+Lanchuii@users.noreply.github.com>
2023-10-12 10:10:40 +0800

    style changes bug fixes for stars

--- src/shared/allReviewsData.ts
+++ src/shared/allReviewsData.ts
@@ -3,7 +3,7 @@ const allReviewData = [
       title: "Arendo",
       address: "Gf - 9 Roli's Arcade, La Salle Avenue",
       image: "https://picsum.photos/350/300",
-      rating: 4,
+      rating: 5,
       price: "3",
       wifiRating: "5",
       reviewData: [
@@ -55,6 +55,36 @@ const allReviewData = [
         {
           Reviewer: "Ziegfred",
           Review: "The first time i went to RTC is that it is very hard to locate at first. The place is inside a building where it is very closed off from the outside. Acquiring a space is cheap and they also offer free drinks inside. Although it is small, You can have a meeting consisting 5-10 people inside the largest space they have. Wi-Fi inside the place is above average since it is not well-known and a few people goes and works there from time to time. Finding food outside the building is not a problem at all since they have karinderyas and small corner shops where you can buy various foods and drinks. "
+        }
+        ]
+    },
+    {
+      title: "Tom n Tom's",
+      address: "2nd F, Perpetual Bldg corner Henaez & Jovita St.", 
+      image: "https://picsum.photos/350/302",
+      rating: 4,
+      price: "3",
+      wifiRating: "3",
+      reviewData: [
+        {
+          Reviewer: "Rex",
+          Review: "Tom-N-Toms was the first cafe I visited in Bacolod and it's still my go-to stop. It has good coffee, lots of tables, plugs and free wifi.  The staff and guards provides great service.The guards makes sure they open the door for me, very helpful when Im carrying a bunch of stuff for work. I tend to go here to work or hold meetings. This was one of the few places that was open during the pandemic. Since I stay home a lot, it provided some releaf. I highly recommend this place. "
+        },
+        {
+          Reviewer: "Lanz",
+          Review: "I've been here several times mostly to do some work and sometimes to hang out with friends. The ambience is pretty cozy although I wouldn't count on their internet too often since it's usually pretty slow and sometimes the drinks I want to order are unavailable. The airconditioning is good though if you want a nice place to just chill out and don't mind the above average coffee prices then this would be a place I would recommend."
+        },
+        {
+          Reviewer: "Joel",
+          Review: "If you're in search of an aesthetically pleasing environment that provides a workspace, especially for students seeking quality coffee while studying, I highly recommend Tom-N-Toms. While their coffee may come at a slightly higher price point, the quality justifies the cost. Moreover, you'll find a conducive atmosphere for studying, socializing, and working, complete with complimentary Wi-Fi and lot of outlets to keep your devices charged. It's worth noting that if you reside in the east part of the city, the commute might be hindered by traffic, but the experience is well worth the effort. I encourage you to check it out."
+        },
+        {
+          Reviewer: "Ariel",
+          Review: "Tom n Toms is one of those cozy spots you stumble upon when you're looking for a good coffee and a chill place to hang out. The vibe is usually relaxed and welcoming, comfy chairs and warm lighting. Now, when it comes to coffee, they've got a decent selection. You can expect your standard espresso options, along with some fancy flavored drinks. Most people find the coffee to be pretty tasty, but it can depend on your personal taste. The bakery side is where Tom n Toms shines. They usually have a range of pastries, sandwiches, and desserts. It's a solid place for breakfast, a quick bite, or a sweet treat to go with your coffee friends and family. Service-wise, they're friendly and helpful. They've got free Wi-Fi, which makes it a decent spot for work or studying. And if you need a power outlet, they often have those available too."
+        },
+        {
+          Reviewer: "Ziegfred",
+          Review: "Tom n Toms is one of my favorite coffee shops in bacolod. The venue of the shop is comfortable and cozy. It gets a little crowded once in a while since it is popular but during off-peak times, you can really relax and enjoy your time. The Wi-Fi speed of the place is average but during peak hours it drastically drops due to internet traffic. You can order a variety of food and beverages since this place does not only serve coffee-based products only. The only downside of Tom n Toms is that the place is in the middle of the main road where traffic is a usual occurance but all in all i highly reccomend you to check it out."
         }
         ]
     }
